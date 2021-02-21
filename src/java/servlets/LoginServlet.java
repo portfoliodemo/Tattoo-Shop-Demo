d4357c077a966a387aae64c1184495b5adb0a781
709488 <709488@SAIT225763IT.ACDM.DS.SAIT.CA>
2021-02-20 23:02:32 -0700

    Log in works better now

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -17,56 +17,64 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author 709488
  */
-
 public class LoginServlet extends HttpServlet {
 
-   
-   
-
-   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
         String userName = request.getParameter("userName");
         String password = request.getParameter("password");
-         //getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
-    String path = getServletContext().getRealPath("/WEB-INF/users.txt");
+        //getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        String path = getServletContext().getRealPath("/WEB-INF/users.txt");
 // to read files
-    BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-    String[] users = null;
-    String line = "";
-    String splitBy = ",";
-    while ((line = br.readLine()) != null){
-        
-        users = line.split(splitBy);
-          for (String x : users){
-        if(x.equals(userName)){
-            request.setAttribute("errorMessage", "You Logged IN");
-            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
-            
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        String[] usernames = null;
+        String[] passwords = null;
+        String line = "";
+        String splitBy = ",";
+        while ((line = br.readLine()) != null) {
+            usernames = line.split(splitBy);
+            for (String x : usernames) {
+                if (x.equals(userName)) {
+                    for (String t : usernames) {
+                        if (t.equals(password)) {
+                            response.sendRedirect("inventory");
+                            session.setAttribute("user_name", userName);
+                        }
+                        else if (t.equals(password) != true) {
+                    request.setAttribute("errorMessage", "Invalid Login");
+                    //getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+                            }
+                       
+                    }
+                   
+                    // getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+                }
+                else if (x.equals(userName) != true) {
+                            request.setAttribute("errorMessage", "Invalid Login");
+                           // getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+     
+                        }
+
+            }
+
         }
-        
-        }
-  
+
     }
-       
-    }
-    
+
 // to append to a file
     //PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, true))); 
-
-    }
-
-
+}
