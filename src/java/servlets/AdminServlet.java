7367567ece8398dc55705c83d4e9a1add56d6c95
709488 <709488@SAIT225763IT.ACDM.DS.SAIT.CA>
2021-02-21 00:02:31 -0700

    Got the admin page to print out the total prices in the file

--- src/java/servlets/AdminServlet.java
+++ src/java/servlets/AdminServlet.java
@@ -5,8 +5,14 @@
  */
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,20 +24,34 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class AdminServlet extends HttpServlet {
 
- 
-
-   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         getServletContext().getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
+        int totalValue = 0;
+        String path = getServletContext().getRealPath("/WEB-INF/homeitems.txt");
+        File file = new File(path);
+        Scanner inFile = new Scanner(file).useDelimiter(",");
+        while (inFile.hasNext()){
+            for (int i = 0; i < 3; i++){
+                inFile.next();
+            }
+            String cost = inFile.next();
+            totalValue += Integer.parseInt(cost);
+            inFile.nextLine();
+            
+        }
+        request.setAttribute("totalValue", totalValue);
+        inFile.close();
+        
+       
+
+        getServletContext().getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         getServletContext().getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
-        
-       
+        getServletContext().getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
+
     }
 }
