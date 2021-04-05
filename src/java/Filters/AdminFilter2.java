31f7869d9c3d5fc146360a964e4c32a52db9d9c2
709488 <709488@SAIT225763IT.ACDM.DS.SAIT.CA>
2021-04-04 19:29:34 -0600

    Added filters

---
+++ src/java/Filters/AdminFilter2.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class AdminFilter2 implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+       
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            
+        //before the servlet
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpSession session = httpRequest.getSession();
+        
+        String user = (String)session.getAttribute("userName");
+        
+        if (user != "admin" && user != "admin2"){
+            HttpServletResponse httpResponse = (HttpServletResponse)response;
+            httpResponse.sendRedirect("login");
+        }
+        
+        
+        chain.doFilter(request, response);
+        
+        // after the servlet
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+    
+    
+}
+    
+   
