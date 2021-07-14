a6fe5bb0ac304496d54376841462d98bed905513
431948 <431948@SAIT222396IT.ACDM.DS.SAIT.CA>
2021-07-14 12:06:56 -0600

    Updates

---
+++ src/java/service/SubscriptionDBException.java
@@ -0,0 +1,17 @@
+
+package service;
+
+/**
+ *
+ * @author 834171
+ */
+public class SubscriptionDBException extends Exception {
+     /**
+     * Sends message from superclass Exception when there is an exception
+     * @param message 
+     */
+    public SubscriptionDBException (String message)
+    {
+        super(message);
+    }
+}
